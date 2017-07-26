7e5817f9827c265eedaa04dfa267e82121b00d6f
dragonet-d <Dragonet_D@163.com>
2017-07-26 13:44:50 +0800

    aa

---
+++ 7-26/base.py
@@ -0,0 +1,22 @@
+a = 100
+if a >= 100:
+  print(a)
+else:
+  print(-a)
+str = 'I\'m ok.'
+print(str)
+b = a
+print(b)
+print('啊啊啊')
+print(ord('A'))
+print(ord('中'))
+print(chr(66))
+print(chr(25911))
+x = b'ABC'
+print(x)
+print('ABC'.encode('ascii'))
+print('中文'.encode('utf-8'))
+print('%2d-%02d' % (3,1))
+print('Hello,%s'%'world')
+print('Hi,%s,you have $%d.'%('Michael',1000000))
+print('小明 %s %d %f'%('你好',250,2.22))
