c575205334fb7947985091c748754d8b28812f05
dragonet_d <Dragonet-D@163.com>
2017-11-01 17:34:54 +0800

    aaa

--- 10-30/文件操作.py
+++ 10-30/文件操作.py
@@ -1,6 +1,6 @@
 # data = open("yesterday",encoding="utf-8").read()
 # f = open("yesterday",'w',encoding="utf-8")
-# r 写
+# r 读
 # w 写 是创建一个文件;会覆盖前面的内容
 # 文件句柄 文件的内存对象
 # print(data)
@@ -21,9 +21,59 @@
 # # 不可 读
 # f.close()
 
-f = open('yesterdays', encoding="utf-8") # 文件句柄
+f = open('yesterdays','r', encoding="utf-8") # 文件句柄
 # for i in range(5):
 #   print(f.readline())
 # print(f.readlines())
+
+#low loop
+'''
 for index,line in enumerate(f.readlines()):
+  if(index == 9):
+    print('-----我是分割线-----')
+    continue
   print(line.strip())
+'''
+
+# high bige
+# 一行一行的读 读完释放内存
+'''
+count = 0
+for line in f:
+  if count == 9:
+    print('--------------')
+    count += 1
+    continue
+  print(line)
+  count += 1
+'''
+
+# 打印焦点的位置
+# 按照字符计数
+print(f.tell())
+print(f.readline().strip())
+print(f.readline().strip())
+print(f.readline().strip())
+print(f.tell())
+# 光标回到某个地方
+f.seek(10)
+print(f.readline().strip())
+print(f.tell())
+# 编码
+print(f.encoding)
+#
+print(f.buffer)
+# errors
+print(f.errors)
+# fileno 返回一个文件句柄在类里面的编号
+print(f.fileno())
+# name
+print(f.name)
+# seekable 如果是字符串是可以移的;
+print(f.seekable())
+# readable 判断文件是否可读
+print(f.readable())
+# writeable
+print(f.writable())
+# flush 默认等到缓存满了再写到内存里面
+print(f.flush())
