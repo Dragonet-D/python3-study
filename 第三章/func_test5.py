c7995529d06ca08395a3e57d0983b8608f550892
dragonet_d <Dragonet-D@163.com>
2017-11-07 14:14:12 +0800

    aaa

---
+++ 第三章/func_test5.py
@@ -0,0 +1,6 @@
+def test(x, y):
+  print(x)
+  print(y)
+
+
+test(1, 3)
--- 第三章/func_text4.py
+++ 第三章/func_text4.py
@@ -3,26 +3,37 @@ def test1():
   print('in the test1')
   return 0
 
+
 test1()
 x = test1()
-print('%s aaaa' %x)
+print('%s aaaa' % x)
+
 
 def test2():
   """函数描述"""
   print('in the test2')
   return 1
 
+
 def test3():
   """函数描述"""
   print('in the test3')
-  return 1, 'hello', ['alex','wupeiqi'], {'name': 'alex'}
+  return 1, 'hello', ['alex', 'wupeiqi'], {'name': 'alex'}
+
+
+def test4():
+  return test2()
+
+
 x = test1()
 z = test3()
 print(z)
 print(type(z))
 y = test2()
+zz = test4()
 print(type(y))
 print(type(x))
+print(test4())
 """
   返回值数=0; 返回None
   返回值数=1; 返回object
