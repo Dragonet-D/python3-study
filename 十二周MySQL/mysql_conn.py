357a9df31c833d42af68345f3eadfef1c67226ea
xiaolong <dragonet_d@163.com>
2018-07-30 21:24:27 +0800

    Aaa

--- 十二周MySQL/mysql_conn.py
+++ 十二周MySQL/mysql_conn.py
@@ -3,4 +3,13 @@ conn = pymysql.connect(host='192.168.0.102', port=3306, user='root', passwd='ale
 # 创建游标
 cursor = conn.cursor()
 effect_row = cursor.execute("select * from student")
-print(effect_row)
+# print(cursor.fetchone())
+
+data = [
+    (5, "xiaolong", "2015-02-12"),
+    (15, "xiaoming", "2015-03-12"),
+    (25, "xiaofeng", "2015-04-12"),
+]
+cursor.executemany("insert into student (age, name, register_date) values (%s, %s,%s)", data)
+conn.commit()
+conn.close()
