b057d75a2421798d27e2d15e0eee35d9eb8dbf27
dragonet-d <Dragonet_D@163.com>
2021-10-01 15:52:27 +0800

    :art:

--- test/日历.py
+++ test/日历.py
@@ -5,3 +5,28 @@ def is_leap_year(year):
         return True
     else:
         return False
+
+
+def getMonthDays(year, month):
+    days = 31
+    if month == 2:
+        if is_leap_year(year):
+            days = 29
+        else:
+            days = 28
+    elif month in [4,6,9,11]:
+        days = 30
+    return days
+
+def getTotalDays(year, month):
+    totalDays = 0
+    for i in range(1990, year):
+        if is_leap_year(i):
+            totalDays += 366
+        else:
+            totalDays += 365
+
+    for i in range(1, month):
+        totalDays += getMonthDays(year, i)
+
+    return totalDays
